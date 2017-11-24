e8a2ec9ec17818dbd586cf9bdc6ec47d81433b28
Anton Geshev <ageshev@despark.com>
2017-11-24 15:43:54 +0200

    Remove some unnecessary logic in observer

--- src/Observers/SeoModelObserver.php
+++ src/Observers/SeoModelObserver.php
@@ -3,31 +3,15 @@
 namespace Despark\Cms\Seo\Observers;
 
 use Despark\Cms\Seo\Contracts\Seoable;
-use Despark\Cms\Seo\Models\Seo;
 
 /**
  * Class SeoModelObserver.
  */
 class SeoModelObserver
 {
-    /**
-     * @var Seo
-     */
-    protected $seoModel;
-
-    /**
-     * SeoModelObserver constructor.
-     *
-     * @param Seo $seoModel
-     */
-    public function __construct(Seo $seoModel)
-    {
-        $this->seoModel = $seoModel;
-    }
-
     public function saving(Seoable $model)
     {
-        $this->seoModel->validate();
+        $model->seo->validate();
     }
 
     /**
@@ -35,13 +19,15 @@ class SeoModelObserver
      */
     public function saved(Seoable $model)
     {
-        if ($model->seo) {
-            $this->seoModel = $model->seo;
-        }
-
         $isEmpty = true;
-
-        $input = request()->only(['meta_title', 'meta_description', 'facebook_title', 'facebook_description', 'twitter_title', 'twitter_description']);
+        $input = request()->only([
+            'meta_title',
+            'meta_description',
+            'facebook_title',
+            'facebook_description',
+            'twitter_title',
+            'twitter_description',
+        ]);
 
         foreach ($input as $key => $value) {
             if (! $value) {
@@ -52,8 +38,8 @@ class SeoModelObserver
         }
 
         if (! $isEmpty || $model->seo) {
-            $this->seoModel->setRawAttributes($input);
-            $model->seo()->save($this->seoModel);
+            $model->seo->setRawAttributes($input);
+            $model->seo()->save($model->seo);
         }
     }
 
